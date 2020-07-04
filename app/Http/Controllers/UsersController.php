259c1d0842b7fecb2299794c49a3147d374d111a
naoya.furumura <hurumura1120@icloud.com>
2020-07-04 08:21:56 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,9 +20,42 @@ class UsersController extends Controller
    public function show($id){
        $user = User::findOrFail($id);
        
+       $user->loadRelationshipCounts();
+       $microposts = $user->microposts()->orderBy('created_at','desc')->paginate(10);
+       
        return view('users.show',[
            'user'=>$user,
+           'microposts'=>$microposts,
            ]);
    }
+   
+   public function followings($id){
+       
+       $user=User::findOrFail($id);
+       
+       $user->loadRelationshipCounts();
+       
+       $followings = $user->followings()->paginate(10);
+       
+       return view('users.followings',[
+           'user'=>$user,
+           'users'=>$followings,
+           ]);
+   }
+   
+   public function followers($id){
+       
+       $user = User::findOrFail($id);
+       
+       $user->loadRelationshipCounts();
+       
+       $followers = $user->followers()->paginate(10);
+       
+       return view('users.followers',[
+           'user'=>$user,
+           'users'=>$followers,
+           ]);
+   }
+   
     
 }
--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -1,20 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="row">
         <aside class="col-sm-4">
-            
             @include('users.card')
         </aside>
         <div class="col-sm-8">
-            
             @include('users.navtabs')
-            @if(Auth::id()==$user->id)
             
-                @include('microposts.form')
-            @endif
-            
-            @include('microposts.microposts')
+            @include('users.users')
         </div>
     </div>
-@endsection   
+@endsection
